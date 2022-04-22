3db17cd87c516790aac21d594e6c05b376242694
adonis7ejeda <adonis7ejeda@gmail.com>
2022-04-21 22:51:55 -0500

    Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+"""
+Write a method that determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
+    return False
